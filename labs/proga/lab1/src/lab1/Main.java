f2d69e1c8df815d04fed9f3a9d9be5e488e89768
Ildar <bnlpn5487@gmail.com>
2025-09-21 18:47:02 +0300

    add private method to fill matrix lab1(proga)

--- labs/proga/lab1/src/lab1/Main.java
+++ labs/proga/lab1/src/lab1/Main.java
@@ -6,9 +6,14 @@ import src.lab1.arrays.MatrixK;
 import src.lab1.utils.Printer;
 
 public class Main {
+    private static final int ARR1_FIRST_VALUE = 3, ARR1_LAST_VALUE = 15;
+
+    private static final float ARR2_MIN_VALUE = -8f, ARR2_MAX_VALUE = 3f;
+    private static final int ARR2_SIZE = 18;
+
     public static void main(String... args) {
-        short[] w = ArrayW.create(3, 15);
-        float[] x = ArrayX.create(-8f, 3f, 18);
+        short[] w = ArrayW.create(ARR1_FIRST_VALUE, ARR1_LAST_VALUE);
+        float[] x = ArrayX.create(ARR2_MIN_VALUE, ARR2_MAX_VALUE, ARR2_SIZE);
         float[][] k = MatrixK.create(w, x);
 
         Printer.printArray(w);
--- labs/proga/lab1/src/lab1/arrays/MatrixK.java
+++ labs/proga/lab1/src/lab1/arrays/MatrixK.java
@@ -8,23 +8,27 @@ public class MatrixK {
             for (int j = 0; j < k[i].length; ++j) {
                 float x = ArrayX[i];
                 float w = ArrayW[j];
-                if (w == 5) {
-                    float result = (float) Math.pow((4 + Math.cos(Math.tan(x))), 2.);
-                    k[i][j] = result;
-                } else if (w == 9 || w == 11 || w == 15) {
-                    float result = (float) (Math.pow(Math.cos(Math.tan(x)),
-                            (Math.exp(Math.cbrt(x)) + 1) / Math.log(Math.pow(2 * Math.abs(x), x))));
-                    k[i][j] = result;
-                } else {
-                    float result = (float) Math.cos(Math.pow(
-                            3.0 - 0.25
-                                    * Math.pow(Math.pow((2.0 / 3.0 - x) / 3.0, x) + 1.0, Math.pow(x / (x + 1.0), 2.0)),
-                            3.0));
-                    k[i][j] = result;
-                }
+                float result = fillMatrix(x, w);
+                k[i][j] = result;
             }
         }
 
         return k;
+    }
+
+    private static float fillMatrix(float x, float w) {
+        float result;
+        if (w == 5) {
+            result = (float) Math.pow((4 + Math.cos(Math.tan(x))), 2.);
+        } else if (w == 9 || w == 11 || w == 15) {
+            result = (float) (Math.pow(Math.cos(Math.tan(x)),
+                    (Math.exp(Math.cbrt(x)) + 1) / Math.log(Math.pow(2 * Math.abs(x), x))));
+        } else {
+            result = (float) Math.cos(Math.pow(
+                    3.0 - 0.25
+                            * Math.pow(Math.pow((2.0 / 3.0 - x) / 3.0, x) + 1.0, Math.pow(x / (x + 1.0), 2.0)),
+                    3.0));
+        }
+        return result;
     }
 }
